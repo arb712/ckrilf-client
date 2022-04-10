4367547b8463fb4b6ef4bdcd9211e1d46bcda935
Riquwu <ariqbimantoro@gmail.com>
2022-04-11 01:50:07 +0700

    Add simple search function

--- src/components/Feeds/Feeds.js
+++ src/components/Feeds/Feeds.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardMedia, Typography, Grid } from "@material-ui/core";
+import {
+  Card,
+  CardMedia,
+  Typography,
+  Grid,
+  TextField,
+} from "@material-ui/core";
+import { Pagination, PaginationItem } from "@material-ui/lab";
 
 import useStyles from "./styles";
 import axios from "axios";
@@ -8,6 +15,7 @@ import { URL } from "../../constants/config";
 const Feeds = () => {
   const classes = useStyles();
   const [feeds, setFeeds] = useState([]);
+  const [query, setQuery] = useState("");
 
   const getAllFeeds = () => {
     axios
@@ -32,22 +40,40 @@ const Feeds = () => {
   }, []);
   return (
     <>
-      {feeds.map((items) => {
-        return (
-          <Grid item xs={12} sm={12} md={6} lg={4}>
-            <Card className={classes.card} raised elevation={6} key={items.id}>
-              <CardMedia
-                className={classes.media}
-                image={items.media.m}
-                title={feeds.title}
-              />
-              <div className={classes.overlay}>
-                <Typography variant="h6">{items.title}</Typography>
-              </div>
-            </Card>
-          </Grid>
-        );
-      })}
+      <TextField
+        name="search"
+        variant="outlined"
+        label="Type something..."
+        fullWidth
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {feeds
+        .filter((items) => {
+          if (query === "") {
+            return items;
+          } else if (
+            items.tags.toLowerCase().includes(query.toLocaleLowerCase())
+          ) {
+            return items;
+          }
+        })
+        .slice()
+        .map((items) => {
+          return (
+            <Grid item xs={12} sm={12} md={6} lg={4} key={items.id}>
+              <Card className={classes.card} raised elevation={6}>
+                <CardMedia
+                  className={classes.media}
+                  image={items.media.m}
+                  title={feeds.title}
+                />
+                <div className={classes.overlay}>
+                  <Typography variant="h6">{items.title}</Typography>
+                </div>
+              </Card>
+            </Grid>
+          );
+        })}
     </>
   );
 };
